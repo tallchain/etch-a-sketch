39cac792024f299dcd25a98a4b9f4f07b738aaa0
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 14:49:39 -0800

    Feat: Randomize colors

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container")
 const h1 = document.querySelector("h1")
-const sqDivCreate = document.createElement("div");
+const sqDivCreate = document.createElement("div")
 const btn = document.querySelector("button")
-sqDivCreate.classList.add("sqDiv");
+sqDivCreate.classList.add("sqDiv")
 let i = 0
 //Create grid
 createGrid()
 function createGrid() {
-let j = prompt("Size of the grid (Enter number from 2-16)")
+let j = prompt("Size of the grid (Enter number from 2-100)")
 for (i = 0; i < (j*j); i++) {
-    sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`;
-    container.append(sqDivCreate.cloneNode(true));
+    sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`
+    container.append(sqDivCreate.cloneNode(true))
 }
 //Color squares from hover
-const sqDivAtt = document.querySelectorAll(".sqDiv");
+const sqDivAtt = document.querySelectorAll(".sqDiv")
+function randomNum() {
+    return Math.floor(Math.random() * 255)
+}
+function getRandomRGB() {
+    return `rgb(${randomNum()} ${randomNum()} ${randomNum()})`
+}
 sqDivAtt.forEach(sq => {
+    let rgb = getRandomRGB()
     sq.style.background = "none"
-    sq.addEventListener('mouseenter', () => sq.style.background = "blue");
-});
+    sq.addEventListener('mouseenter', () => {sq.style.background = rgb})
+})
 //Restart
 btn.addEventListener("click", () => {
-    sqDivAtt.forEach(sq => {container.removeChild(sq)});
+    sqDivAtt.forEach(sq => {container.removeChild(sq)})
     createGrid();
-})}
+})
+}
+
