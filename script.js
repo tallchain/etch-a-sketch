7bd29c0ea939f95520c4c8b628f0790959f8e9a5
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 02:17:40 -0800

    Feat: Color tiles on hover

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const sqDiv = document.createElement("div");
 sqDiv.classList.add("sqDiv")
 
 let i = 0
-let j = prompt("tiles")
+let j = prompt("Size of the grid (Enter number from 2-16)")
+
 for (i = 0; i < (j*j); i++) {
-sqDiv.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`;
-
-container.append(sqDiv.cloneNode(true))
-console.log(sqDiv.style.cssText = `width: ${j}%; height: ${j}%`)
-
+    sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`;
+    container.append(sqDivCreate.cloneNode(true));
 }
+const sqDivAtt = document.querySelectorAll(".sqDiv");
+sqDivAtt.forEach(sq => {
+    sq.style.background = "none"
+    sq.addEventListener('mouseenter', () => sq.style.background = "blue");
+});
