f7a6fb7135bdeac17ac534d5a5fb87518a32a5db
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 14:27:33 -0800

    Feat: Create restart button
    User replaces current grid with a new un-marked grid.

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const container = document.querySelector(".container")
-const sqDiv = document.createElement("div");
-sqDiv.classList.add("sqDiv")
-
+const container = document.querySelector(".container");
+const h1 = document.querySelector("h1")
+const sqDivCreate = document.createElement("div");
+const btn = document.querySelector("button")
+sqDivCreate.classList.add("sqDiv");
 let i = 0
+//Create grid
+createGrid()
+function createGrid() {
 let j = prompt("Size of the grid (Enter number from 2-16)")
-
 for (i = 0; i < (j*j); i++) {
     sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`;
     container.append(sqDivCreate.cloneNode(true));
 }
+//Color squares from hover
 const sqDivAtt = document.querySelectorAll(".sqDiv");
 sqDivAtt.forEach(sq => {
     sq.style.background = "none"
     sq.addEventListener('mouseenter', () => sq.style.background = "blue");
 });
+//Restart
+btn.addEventListener("click", () => {
+    sqDivAtt.forEach(sq => {container.removeChild(sq)});
+    createGrid();
+})}
