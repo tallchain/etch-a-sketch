454f676d25911cae8c8bdd2bd78c5357e7097e23
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 17:28:13 -0800

    Feat: Add progressive darkening effect
    -Hovering multiple times over a square will darken its color by 10%.
    -Removed border because it looks cooler without it.
    -Removed h1 selector and background="none" because they do nothing.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector(".container")
-const h1 = document.querySelector("h1")
 const sqDivCreate = document.createElement("div")
 const btn = document.querySelector("button")
 sqDivCreate.classList.add("sqDiv")
@@ -7,28 +6,30 @@ let i = 0
 //Create grid
 createGrid()
 function createGrid() {
-let j = prompt("Size of the grid (Enter number from 2-100)")
-for (i = 0; i < (j*j); i++) {
-    sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`
-    container.append(sqDivCreate.cloneNode(true))
+    let j = prompt("Size of the grid (Enter number from 2-100)")
+    for (i = 0; i < (j*j); i++) {
+        sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`
+        container.append(sqDivCreate.cloneNode(true))
+        }
+    //Color squares from hover
+    const sqDivAtt = document.querySelectorAll(".sqDiv")
+    function randomNum() {
+        return Math.floor(Math.random() * 255)
+        }
+    function getRandomRGB() {
+        return `rgb(${randomNum()} ${randomNum()} ${randomNum()}`
+        }
+    sqDivAtt.forEach(sq => {
+        let rgb = getRandomRGB();
+        let opacity = 0
+        function addOpacity() {
+            return opacity += 0.1
+        }
+        sq.addEventListener('mouseenter', () => {sq.style.background = `${rgb} / ${addOpacity()})`;}
+            )})
+    //Restart
+    btn.addEventListener("click", () => {
+        sqDivAtt.forEach(sq => {container.removeChild(sq)})
+        createGrid();
+        })
 }
-//Color squares from hover
-const sqDivAtt = document.querySelectorAll(".sqDiv")
-function randomNum() {
-    return Math.floor(Math.random() * 255)
-}
-function getRandomRGB() {
-    return `rgb(${randomNum()} ${randomNum()} ${randomNum()})`
-}
-sqDivAtt.forEach(sq => {
-    let rgb = getRandomRGB()
-    sq.style.background = "none"
-    sq.addEventListener('mouseenter', () => {sq.style.background = rgb})
-})
-//Restart
-btn.addEventListener("click", () => {
-    sqDivAtt.forEach(sq => {container.removeChild(sq)})
-    createGrid();
-})
-}
-
