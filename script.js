9a704946cd29acbd8d6c5b1968833bb3a64c6195
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 22:45:26 -0800

    Style: Restrict color palette / style container

--- script.js
+++ script.js
@@ -5,40 +5,37 @@ sqDivCreate.classList.add("sqDiv")
 let i = 0
 
 //Create grid
-
-createGrid(10)
+createGrid(20)
 function createGrid(j) {
     for (i = 0; i < (j*j); i++) {
         sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`
         container.append(sqDivCreate.cloneNode(true))
-        }
-    
+    }
     //Color squares from hover
-const sqDivAtt = document.querySelectorAll(".sqDiv")
-function randomNum() {
-    return Math.floor(Math.random() * 255)
+    const sqDivAtt = document.querySelectorAll(".sqDiv")
+    function randomNum(min, max) {
+        return Math.floor(Math.random() * (max-min) + min)
     }
-function getRandomRGB() {
-    return `rgb(${randomNum()} 
-                ${randomNum()} 
-                ${randomNum()}`
+    function getRandomRGB() {
+        return `rgb(${randomNum(200,255)} 
+                    ${randomNum(100,255)} 
+                    ${randomNum(150,255)}`
     }
-sqDivAtt.forEach(sq => {
-    let rgb = getRandomRGB();
-    let opacity = 0
-    function addOpacity() {
-        return opacity += 0.2
-        }
-    sq.addEventListener('mouseenter', () => {
-        sq.style.background = `${rgb} / ${addOpacity()})`;
+    sqDivAtt.forEach(sq => {
+        let rgb = getRandomRGB();
+        let opacity = 0
+        const addOpacity = () => opacity += 0.4;
+        sq.addEventListener('mouseenter', () => {
+            sq.style.background = `${rgb} / ${addOpacity()})`;
         })
-        })
-    //Restart
+    })
+        //Restart
     btn.addEventListener("click", () => {
         sqDivAtt.forEach(sq => {
             container.removeChild(sq)});
-        prompted = prompt("Size of the grid (Enter number from 2-100)");
-        createGrid(prompted);})
+        tiles = prompt("Size of the grid (Enter number from 2-100)");
+        createGrid(tiles);
+    })
 }
         
 
