ae654aa39f43f9fbaf9442579483ac3506bada98
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-29 22:09:34 -0800

    Feat: Default to 20x20 on open
    Allows for a more frictionless user experience because a key input isn't
    required.

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ const sqDivCreate = document.createElement("div")
 const btn = document.querySelector("button")
 sqDivCreate.classList.add("sqDiv")
 let i = 0
+
 //Create grid
-createGrid()
-function createGrid() {
-    let j = prompt("Size of the grid (Enter number from 2-100)")
+
+createGrid(10)
+function createGrid(j) {
     for (i = 0; i < (j*j); i++) {
         sqDivCreate.style.cssText = `width: ${(100/j)}%; height: ${100/j}%`
         container.append(sqDivCreate.cloneNode(true))
         }
+    
     //Color squares from hover
-    const sqDivAtt = document.querySelectorAll(".sqDiv")
-    function randomNum() {
-        return Math.floor(Math.random() * 255)
+const sqDivAtt = document.querySelectorAll(".sqDiv")
+function randomNum() {
+    return Math.floor(Math.random() * 255)
+    }
+function getRandomRGB() {
+    return `rgb(${randomNum()} 
+                ${randomNum()} 
+                ${randomNum()}`
+    }
+sqDivAtt.forEach(sq => {
+    let rgb = getRandomRGB();
+    let opacity = 0
+    function addOpacity() {
+        return opacity += 0.2
         }
-    function getRandomRGB() {
-        return `rgb(${randomNum()} ${randomNum()} ${randomNum()}`
-        }
-    sqDivAtt.forEach(sq => {
-        let rgb = getRandomRGB();
-        let opacity = 0
-        function addOpacity() {
-            return opacity += 0.1
-        }
-        sq.addEventListener('mouseenter', () => {sq.style.background = `${rgb} / ${addOpacity()})`;}
-            )})
+    sq.addEventListener('mouseenter', () => {
+        sq.style.background = `${rgb} / ${addOpacity()})`;
+        })
+        })
     //Restart
     btn.addEventListener("click", () => {
-        sqDivAtt.forEach(sq => {container.removeChild(sq)})
-        createGrid();
-        })
+        sqDivAtt.forEach(sq => {
+            container.removeChild(sq)});
+        prompted = prompt("Size of the grid (Enter number from 2-100)");
+        createGrid(prompted);})
 }
+        
+
+
